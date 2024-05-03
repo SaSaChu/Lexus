b15467775700280fe3871ef22ce667e55467715f
SaSaChu <teresa7397@gmail.com>
2024-05-03 09:37:35 +0800

    首頁一半

--- js/public.js
+++ js/public.js
@@ -6,7 +6,7 @@ $(function () {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 4500,
       disableOnInteraction: false,
     },
     pagination: {

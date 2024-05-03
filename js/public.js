225d65df7aaff27feb79007691dcf99a6c1a0aba
SaSaChu <teresa7397@gmail.com>
2024-05-03 17:49:35 +0800

    完成x5

--- js/public.js
+++ js/public.js
@@ -19,4 +19,15 @@ $(function () {
     },
   });
 
+  var myModal = document.getElementById('myModal')
+  var myInput = document.getElementById('myInput')
+
+  myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+  });
+
 });
+
+
+
+

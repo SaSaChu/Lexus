36bc4b27f2ff75a6c00ae12225251eba28636fa2
CHO <bsuysheep927@gmail.com>
2024-05-07 13:28:23 +0800

    hank 修復首頁 swiper

--- js/public.js
+++ js/public.js
@@ -1,6 +1,7 @@
 $(function () {
 
-  $('.modus-img').imgLiquid ();
+  // 這段不知道要幹嘛，會出現錯誤，導致後面的 js 無法運行，先隱藏
+  // $('.modus-img').imgLiquid ();
 
   var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -18,6 +19,33 @@ $(function () {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // 0507 swiper Tag;
+  var swiperTag = new Swiper('.swiper-tag', {
+    slidesPerView: 1,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next-tag',
+      prevEl: '.swiper-button-prev-tag',
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 7,
+      }, 
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      },
+    },
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
 
   var myModal = document.getElementById('myModal')
   var myInput = document.getElementById('myInput')

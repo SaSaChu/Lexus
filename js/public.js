b76a93f29186a3105e8d9934b5b0ef54acbe98fe
SaSaChu <teresa7397@gmail.com>
2024-05-07 14:11:47 +0800

    套入類別

--- js/public.js
+++ js/public.js
@@ -1,51 +1,6 @@
 $(function () {
 
-  // 這段不知道要幹嘛，會出現錯誤，導致後面的 js 無法運行，先隱藏
-  // $('.modus-img').imgLiquid ();
-
-  var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  // 0507 swiper Tag;
-  var swiperTag = new Swiper('.swiper-tag', {
-    slidesPerView: 1,
-    direction: getDirection(),
-    navigation: {
-      nextEl: '.swiper-button-next-tag',
-      prevEl: '.swiper-button-prev-tag',
-    },
-    breakpoints: {
-      1024: {
-        slidesPerView: 7,
-      }, 
-    },
-    on: {
-      resize: function () {
-        swiper.changeDirection(getDirection());
-      },
-    },
-  });
-
-  function getDirection() {
-    var windowWidth = window.innerWidth;
-    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-    return direction;
-  }
+  $('.modus-img').imgLiquid ();
 
   var myModal = document.getElementById('myModal')
   var myInput = document.getElementById('myInput')
